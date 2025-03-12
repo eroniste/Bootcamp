d92a2307546a97533428790aec5b0b945d19c3de
eroniste <ilyass.sboul.12@gmail.com>
2025-03-12 13:04:12 +0000

    day3

---
+++ week1/day3/exGold1.py
@@ -0,0 +1,20 @@
+import math
+
+class Circle:
+    def __init__(self, radius=1.0):
+        self.radius = radius
+    
+    def perimeter(self):
+        return 2 * math.pi * self.radius
+    
+    def area(self):
+        return math.pi * self.radius ** 2
+    
+    def definition(self):
+        print("A circle is a shape consisting of all points in a plane that are at a given distance from a fixed center point.")
+
+# Example usage
+circle = Circle(5)
+print("Perimeter:", circle.perimeter())
+print("Area:", circle.area())
+circle.definition()
