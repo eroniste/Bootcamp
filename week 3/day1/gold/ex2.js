703ac7fc520f83350912b1215b68072f4af15b7d
eroniste <ilyass.sboul.12@gmail.com>
2025-04-02 12:24:50 +0000

    week3

---
+++ week 3/day1/gold/ex2.js
@@ -0,0 +1,18 @@
+// attendance.js
+
+let guestList = {
+    randy: "Germany",
+    karla: "France",
+    wendy: "Japan",
+    norman: "England",
+    sam: "Argentina"
+  };
+  
+  let studentName = prompt("What is your name?");
+  
+  if (studentName in guestList) {
+      console.log(`Hi! I'm ${studentName}, and I'm from ${guestList[studentName]}.`);
+  } else {
+      console.log("Hi! I'm a guest.");
+  }
+  
